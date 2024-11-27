1dae3d620129cccc79868d2db866552e20f1b3dd
eric2054 <90565230+eric2054@users.noreply.github.com>
2024-11-27 13:55:04 +0800

    Update 5864.cpp

--- 5864.cpp
+++ 5864.cpp
@@ -1,7 +1,7 @@
-#include <stdio.h>
 #include <iostream>
+#include <stack>
+#include <string>
 using namespace std;
-
 
 int Priority(char op) {
     switch (op) {
@@ -16,59 +16,44 @@ int Priority(char op) {
     }
 }
 
+void InfixToPrefix(const string& infix) {
+    stack<char> operators;
+    string result;
 
-void InfixToPrefix(char infix[]) {
-    char Postfix[20] = {};      // ?ন??Ǫ?Postfix
-    char symbol_temp[10] = {};  // ???I?Ÿ????s??
-    int current_infix = 0, symbol_top = -1, postfix_top = -1;
+    for (int i = infix.length() - 1; i >= 0; --i) {
+        char current = infix[i];
 
-    for (int i = 0; infix[i] != '\0'; i++) {
-        current_infix++;
-    }
-
-    while (infix[--current_infix]) {
-        switch (infix[current_infix]) {
-           
-            case ')':
-                symbol_temp[++symbol_top] = infix[current_infix];
-                break;  // case ')':
-
-          
-            case '+':
-            case '-':
-            case '*':
-            case '/':
-                
-                while (Priority(infix[current_infix]) <
-                       Priority(symbol_temp[symbol_top])) {
-                    Postfix[++postfix_top] = symbol_temp[symbol_top--];
-                }
-                // ??{?b????i?h
-                symbol_temp[++symbol_top] = infix[current_infix];
-                break;  // case '+': case '-': case '*': case '/':
-
-            case '(':
-                while (symbol_temp[symbol_top] != ')')
-                    Postfix[++postfix_top] = symbol_temp[symbol_top--];
-                symbol_top--;
-                break;  //  case ')':
-
-            default:
-                Postfix[++postfix_top] = infix[current_infix];
+        if (isdigit(current) || isalpha(current)) {
+            result += current;
+        } else if (current == ')') {
+            operators.push(current);
+        } else if (current == '(') {
+            while (!operators.empty() && operators.top() != ')') {
+                result += operators.top();
+                operators.pop();
+            }
+            operators.pop();
+        } else {
+            while (!operators.empty() && Priority(current) < Priority(operators.top())) {
+                result += operators.top();
+                operators.pop();
+            }
+            operators.push(current);
         }
-
-    } 
-
-    while (symbol_temp[symbol_top]) {
-        Postfix[++postfix_top] = symbol_temp[symbol_top--];
     }
 
-    for (int i = postfix_top; i >= 0; i--)
-        cout << Postfix[i];
+    while (!operators.empty()) {
+        result += operators.top();
+        operators.pop();
+    }
+
+    reverse(result.begin(), result.end());
+    cout << result << endl;
 }
 
-int main(int argc, char* argv[]) {
-    char itemset[20];
-    scanf("%s", itemset);
+int main() {
+    string itemset;
+    cin >> itemset;
     InfixToPrefix(itemset);
+    return 0;
 }
